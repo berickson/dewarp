ca538a27e7e50a1edb89a0cf231297708c6ac9a0
Brian Erickson <berickson@gmail.com>
2018-04-03 17:21:28 +0200

    added move, .gitignore

--- dewarp.cpp
+++ dewarp.cpp
@@ -11,10 +11,26 @@ public:
     double x;
     double y;
     double theta;
+
+    Pose() : x(0), y(0), theta(0) {
+
+    }
+    Pose(Eigen::Vector2d p, double theta) : Pose(p(0), p(1), theta) {
+
+    }
+
     Pose(double x, double y, double theta) : x(x), y(y), theta(theta) {
     }
 
-    Eigen::Vector2d Pose2World(Eigen::Vector2d p) {
+    Pose move(double dx, double dy, double dtheta) {
+        Eigen::Vector2d p(dx, dy);
+        auto w = Pose2World(p);
+        x = w(0);
+        y = w(1);
+        theta += dtheta;
+    }
+
+    Eigen::Vector2d Pose2World(Eigen::Vector2d p) const {
         Eigen::Vector2d rv;
         rv(0) = p(0) * cos(theta) - p(1) * sin(theta) + x;
         rv(1) = p(0) * sin(theta) + p(1) * cos(theta) + y;
@@ -22,14 +38,14 @@ public:
         return rv;
     }
 
-    Eigen::Transform<double,2,Eigen::Affine> Pose2WorldTransform() {
+    Eigen::Transform<double,2,Eigen::Affine> Pose2WorldTransform() const {
         Eigen::Transform<double,2,Eigen::Affine> t;
         t.setIdentity();
         t.rotate(theta);
         t.translate(Eigen::Vector2d(x,y));
         return t;
     }
-    Eigen::Transform<double,2,Eigen::Affine> World2PoseTransform() {
+    Eigen::Transform<double,2,Eigen::Affine> World2PoseTransform() const {
         return Pose2WorldTransform().inverse();
     }
 };
@@ -57,5 +73,11 @@ int main(int, char**)
     cout << "untransform_p :" << wp << endl;
     //cout << "tranform :" << pose.Pose2WorldTransform() << endl;
     cout << "done" << endl;
+
+    Pose p2;
+    for(int i =  0; i<11; i++) {
+        cout << "i:" << i << " pose: " << to_string(p2) << endl;
+        p2.move(1,0, 2. * EIGEN_PI / 10.);
+    }
     return 0;
 }
